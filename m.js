e509484f01c1c320aeb1141a261a54d9db790af4
miftahul munir <miftahulmunir001@gmail.com>
2023-02-12 11:53:34 +0700

    Update m.js

--- m.js
+++ m.js
@@ -1,4 +1,4 @@
-const el= a =>{
+const el = a=>{
 	a.g = [
 		document,
 		Object,
@@ -8,44 +8,142 @@ const el= a =>{
 		'forEach',
 		'keys',
 		'setAttribute',
-		'textContent',
+		'textContent'
 	]
 	a.f = a.g[0][a.g[4]](a.a)
 	a.c && (a.f[a.g[8]] = a.c)
-	a.d && a.g[1][a.g[6]](a.d)[a.g[5]](d => {
-			a.f[a.g[7]](d, a.d[d])
-		})
-	a.e && a.g[1][a.g[6]](a.e)[a.g[5]](e => {
-			a.f[a.g[2]](e, a.e[e])
-		})
+	a.d && a.g[1][a.g[6]](a.d) [a.g[5]](d=>{
+		a.f[a.g[7]](d, a.d[d])
+	})
+	a.e && a.g[1][a.g[6]](a.e) [a.g[5]](e=>{
+		a.f[a.g[2]](e, a.e[e])
+	})
 	a.b && a.b[a.g[3]](a.f)
 	return a.f
-};
-const dlg=a=>{
-	(a=a||{}).a=[document,window,'addEventListener','appendChild','body','clientX','clientY','event','lastElementChild','left','mousemove','mouseup','offsetLeft','offsetTop','parentElement','preventDefault','removeChild','removeEventListener','stopPropagation','style','target','top']
-	const b=[
-		a.a,0,0,
-		el({a:'div', b:a.a[0][a.a[4]], d:{style:`top:${a?a.top||16:16}px;left:${a?a.left||32:32}px;background:rgba(255,255,255,.6);border-radius:8px;box-shadow:0 0 6px 2px rgba(0,0,0,.1);position:fixed;padding:12px 16px 16px 16px;`}, e:{mousedown:e=>{e[b[0][18]]();b[4]()}}}),
-		a=>{b[0][0][b[0][4]][b[0][8]]!==b[3]&&b[0][0][b[0][4]][b[0][3]](b[3])},
-		a=>{
-			a=a||b[0][1][b[0][7]]
-			a[b[0][15]]()
-			a[b[0][18]]()
-			b[1]=a[b[0][5]]
-			b[2]=a[b[0][6]]
-			b[4]()
-			b[0][0][b[0][2]](b[0][11],b[7])
-			b[0][0][b[0][2]](b[0][10],b[6])
+}
+const dlg = a=>{
+	a = [
+		a ? a.left || 32 : 32,
+		a ? a.top || 16 : 16,
+		b=>{
+			a[7][a[14]][a[29]] != a[6] && a[7][a[14]][a[12]](a[6])
 		},
-		a=>{
-			a=a||b[0][1][b[0][7]]
-			a[b[0][15]]()
-			b[3][b[0][19]][b[0][21]]=(b[3][b[0][13]]-b[2]+a[b[0][6]])+"px"
-			b[3][b[0][19]][b[0][9]]=(b[3][b[0][12]]-b[1]+a[b[0][5]])+"px"
-			b[1]=a[b[0][5]]
-			b[2]=a[b[0][6]]
+		b=>{
+			b = b || a[8][a[24]]
+			b[a[49]]()
+			b[a[40]]()
+			a[0] = b[a[19]]
+			a[1] = b[a[20]]
+			a[2]()
+			a[7][a[10]](a[33], a[5])
+			a[7][a[10]](a[32], a[4])
 		},
-		a=>{b[0][0][b[0][17]](b[0][11],b[7]);b[0][0][b[0][17]](b[0][10],b[6])},
+		b=>{
+			b = b || a[8][a[24]]
+			b[a[49]]()
+			a[6][a[50]][a[30]] = (a[6][a[34]] - a[0] + b[a[19]]) + a[41]
+			a[6][a[50]][a[53]] = (a[6][a[35]] - a[1] + b[a[20]]) + a[41]
+			a[0] = b[a[19]]
+			a[1] = b[a[20]]
+		},
+		b=>{
+			a[7][a[44]](a[32], a[4])
+			a[7][a[44]](a[33], a[5])
+		},,
+		document,
+		window,
+		'absolute',
+		'addEventListener',
+		'align',
+		'appendChild',
+		'background',
+		'body',
+		'both',
+		'box',
+		'border',
+		'center',
+		'clientX',
+		'clientY',
+		'cursor',
+		'default',
+		'div',
+		'event',
+		'fixed',
+		'font',
+		'grab',
+		'height',
+		'lastElementChild',
+		'left',
+		'margin',
+		'mousemove',
+		'mouseup',
+		'offsetLeft',
+		'offsetTop',
+		'overflow',
+		'padding',
+		'parentElement',
+		'position',
+		'preventDefault',
+		'px',
+		'radius',
+		'removeChild',
+		'removeEventListener',
+		'resize',
+		'rgba',
+		'scroll',
+		'shadow',
+		'stopPropagation',
+		'style',
+		'target',
+		'text',
+		'top',
+		'weight',
+		'width',
+		a ? a.title || 'title' : 'title',
+		a ? a.width || 300 : 300,
+		a ? a.height || 200 : 200
 	]
-	return {b:b[3],ct:el({a:'div', b:el({a:'div', b:el({a:'h4', c:a?a.title||'title':'title', b:b[3], d:{style:'background:rgba(0,0,0,.8);border-radius:6px;color:rgba(255,255,255,.8);cursor:grab;margin:0 0 12px 0;padding:8px 48px 8px 32px;text-align:center;'}, e:{mousedown:b[5]}})[b[0][14]], c:'x', d:{style:'background:rgba(255,255,255,.8);border-radius:4px;cursor:default;font-weight:bold;padding:4px;position:absolute;text-align:center;top:16px;right:22px;'}, e:{click:e=>{e[b[0][18]]();b[0][0][b[0][4]][b[0][16]](e[b[0][20]][b[0][14]])}}})[b[0][14]], d:{style:`width:${a?a.width||400:400}px;height:${a?a.height||320:320}px;background:rgba(255,255,255,.8);border:1px solid rgba(0,0,0,.1);border-radius:8px;padding:8px;min-width:80px;min-height:48px;resize:both;overflow:scroll;`}})}
+	a[6] = el({
+		a: a[23],
+		b: a[7][a[14]],
+		d: {
+			style: `${ a[53] }:${ a[1] + a[41] };${ a[30] }:${ a[0] + a[41] };${ a[13] }:${ a[46] }(255,255,255,.6);${ a[17] }-${ a[42] }:8${ a[41] };${ a[16] }-${ a[48] }:0 0 6${ a[41] } 2${ a[41] } ${ a[46] }(0,0,0,.1);${ a[39] }:${ a[25] };${ a[37] }:12${ a[41] } 16${ a[41] } 16${ a[41] } 16${ a[41] };`
+		},
+		e: {
+			mousedown: e=>{
+				e[a[49]]()
+				a[2]()
+			}
+		}
+	})
+	return el({
+		a: a[23],
+		b:
+		el({
+			a: a[23],
+			b:
+			el({
+				a: a[23],
+				b: a[6],
+				c: a[56],
+				d: {
+					style:`${ a[13] }:${ a[46] }(0,0,0,.8);${ a[17] }-${ a[42] }:6${ a[41] };color:${ a[46] }(255,255,255,.8);${ a[21] }:${ a[27] };${ a[31] }:0 0 12${ a[41] } 0;${ a[37] }:8${ a[41] } 48${ a[41] } 8${ a[41] } 32${ a[41] };${ a[52] }-${ a[11] }:${ a[18] };`
+				},
+				e: { mousedown: a[3] }
+			}) [a[38]],
+		c: 'x',
+		d: {
+			style: `${ a[13] }:${ a[46] }(255,255,255,.8);${ a[17] }-${ a[42] }:4${ a[41] };${ a[21] }:${ a[22] };${ a[26] }-${ a[54] }:bold;${ a[37] }:4${ a[41] };${ a[39] }:${ a[9] };${ a[52] }-${ a[11] }:${ a[18] };${ a[53] }:16${ a[41] };right:22${ a[41] };`
+		},
+		e: {
+			click: e=>{
+				e[a[49]]()
+				a[7][a[14]][a[43]](a[6])
+			}
+		}
+		}) [a[38]],
+		d: {
+			style: `${ a[55] }:${ a[57] }${ a[41] };${ a[28] }:${ a[58] }${ a[41] };${ a[13] }:${ a[46] }(255,255,255,.8);${ a[17] }:1${ a[41] } solid ${ a[46] }(0,0,0,.1);${ a[17] }-${ a[41] }:8${ a[41] };${ a[37] }:8${ a[41] };min-${ a[55] }:80${ a[41] };min-${ a[28] }:48${ a[41] };${ a[45] }:${ a[15] };${ a[36] }:${ a[47] };`
+		}
+	})
 }
